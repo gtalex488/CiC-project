7376da6f19497bf12623de7570182607930d6f64
gtalex488 <promobox12@yahoo.ca>
2025-03-05 13:27:36 -0500

    initial commit

---
+++ test.js
@@ -0,0 +1,37 @@
+/*age = 500
+if (age >= 0 && age <=12) {console.log("Child")}
+else if (age >=13 && age <=17) {console.log("Teenager")}
+else if (age >=18 && age <= 64) {console.log("Adult")}
+else if (age >=65) {console.log("Senior")}
+else
+{console.log("invalid age")}*/
+
+/*let day = 'weekday'
+let weather = 'sunny'
+if (day == 'weekend' && weather == 'sunny') 
+{console.log("go for picnic")}
+else
+{console.log("stay home!")}
+
+weekend = true
+sunny = true
+if (weekend && sunny) 
+{console.log("go for picnic")}
+else
+{console.log("stay home!")}*/
+
+libraryopen = true;
+nochores = true
+if (libraryopen && nochores) 
+{console.log("study at the library")}
+else {console.log("cannot go to library")}
+
+
+
+
+
+
+
+
+
+
